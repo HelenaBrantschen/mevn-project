57c9cb0f4b02d5f02404dd4d435573f696f2048c
Helena <mariahelena.brantschen@stud.hslu.ch>
2021-05-13 14:42:02 +0200

    [fix] server-2 [init] routes.js create Post

---
+++ server-2/controllers/api.js
@@ -0,0 +1,37 @@
+const Post = require("../models/posts");
+
+module.exports = class API {
+    //fetch all posts
+    static async fetchAllPosts(req, res){
+        try{
+            const posts = await Post.find();
+            res.status(200).json(posts);
+        }catch (err){
+            res.status(404).json({message: err.message})
+        }
+    };
+    //fetch postby id 
+    static async fetchPostById(req, res){
+        res.send('Fetch post by Id');
+    };
+      //create a post 
+      static async createPost(req, res){
+        const post = req.body;
+        const imagename = req.file.filename;
+        post.image = imagename;
+        try{
+            await Post.create(post);
+            res.status(201).json({message: 'Post sucessfully created'})
+        } catch (err){
+            res.status(400).json({message: err.message})
+        }
+    };
+       //update a post 
+       static async updatePost(req, res){
+        res.send('update a post');
+    };
+       //delete a post 
+       static async deletePost(req, res){
+        res.send('delete a post');
+    };
+};
---
+++ server-2/routes/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const API = require('../controllers/api')
+const multer = require('multer')
+
+
+// multer middleware for image upload
+let storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
+    },
+});
+
+let upload = multer({
+    storage: storage
+}).single('image');
+
+
+router.get('/', API.fetchAllPosts); 
+router.get('/:id', API.fetchPostById); 
+router.post('/', upload, API.createPost); 
+router.patch('/:id', API.updatePost);
+router.delete('/:id', API.deletePost);
+
+
+
+module.exports = router;
