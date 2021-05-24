4b4c10be53ed31dc489601ac2270015ba4187334
Nick Niles <nick@nickniles.com>
2021-05-24 15:06:32 +0200

    [feat] format date

--- client-2/src/main.js
+++ client-2/src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import moment from 'moment'
+
+Vue.filter('formatDate', function(value) {
+    if (value) {
+        return moment(String(value)).format('DD.MM.YYYY')
+    }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
--- client-2/src/views/Post.vue
+++ client-2/src/views/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <img class="header-img" :src="`/${post.image}`" alt="" />
-    <p class="date">{{ date }}</p>
+    <p class="date">{{ post.created | formatDate}}</p>
     <h1>{{ post.title }}</h1>
     <p>{{ post.content }}</p>
 
@@ -13,33 +13,7 @@
     >
     <v-btn color="red" text @click="removePost(post._id)">Delete</v-btn>
   </div>
-  <!-- <v-container>
-    <v-row>
-      <v-col sm="10" class="pa-4 mx-auto">
-        <v-card class="pa-2">
-          <v-img :src="`/${post.image}`"></v-img>
-          <v-card-actions class="pb-0">
-              <v-row class="mt-1 mx-1">
-                  <v-col sm="2">
-                      <v-btn small outlined color="primary"> {{ post.category }}</v-btn>
-                  </v-col>
-                  <v-col sm="10" class="d-flex justify-end">
-                      <v-btn color="success" text :to ="{name: 'edit-post', params: { id: post._id }}">Edit</v-btn>
-                    <v-btn color="red" text @click="removePost(post._id)">Delete</v-btn>
-                  </v-col>
-              </v-row>
-          </v-card-actions>
-          <v-card-subtitle class="headline">
-              <h3>{{ post.title }} </h3>
-          </v-card-subtitle>
-          <v-card-text class="grey--text">
-              <p>{{ post.content }}</p>
-              <p>{{ post.created }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container> -->
+  
 </template>
 <style scoped>
 .header-img{
