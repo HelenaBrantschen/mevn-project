dded06cebabb70b979dde2c70d03aaa6acff2cab
Helena <mariahelena.brantschen@stud.hslu.ch>
2021-11-11 21:47:24 +0100

    [upadate] component imports

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,16 +5,26 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {GalleryComponent} from "./components/gallery/gallery.component";
 import {HttpClientModule} from "@angular/common/http";
+import { RoundbtnComponent } from './components/roundbtn/roundbtn.component';
+import { MapComponent } from './components/map/map.component';
+import { BtnNavComponent } from './components/btn-nav/btn-nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
     declarations: [
         AppComponent,
-        GalleryComponent
+        GalleryComponent,
+        RoundbtnComponent,
+        MapComponent,
+        BtnNavComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule,
+        OverlayModule
     ],
     providers: [],
     bootstrap: [AppComponent]
