9323efb03ecd104ff95d8eee83355d8e16c34116
Helena <mariahelena.brantschen@stud.hslu.ch>
2021-11-12 00:51:34 +0100

    [remove] unused on init

--- angular-client/src/app/components/btn-nav/btn-nav.component.ts
+++ angular-client/src/app/components/btn-nav/btn-nav.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'btn-nav',
   templateUrl: './btn-nav.component.html',
   styleUrls: ['./btn-nav.component.scss']
 })
-export class BtnNavComponent implements OnInit {
+export class BtnNavComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
