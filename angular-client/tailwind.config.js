1a71ee677bc68f7d7cd75c8d77772571375f772c
Helena <mariahelena.brantschen@stud.hslu.ch>
2021-12-29 17:47:35 +0100

    custom screenbreakpoints

--- angular-client/tailwind.config.js
+++ angular-client/tailwind.config.js
@@ -31,6 +31,22 @@ module.exports = {
         900: '#164e63',
       },
     },
+    screens:{'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1080px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+
+    },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
