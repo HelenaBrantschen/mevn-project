f9c85ab64a7de282b51bc3c1d3b79966113b79cf
Helena <mariahelena.brantschen@stud.hslu.ch>
2022-01-08 11:04:12 +0100

    [fix] calendeer ng deep, icons,savePost formdata

--- angular-client/src/app/services/http.service.ts
+++ angular-client/src/app/services/http.service.ts
@@ -1,3 +1,4 @@
+import { Router, RouterModule } from '@angular/router';
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
@@ -6,10 +7,19 @@ import {HttpClient} from "@angular/common/http";
 })
 export class HttpService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) 
+              { }
 
   async loadPosts() {
     var test = await this.http.get("http://localhost:5000/api/post");
     return test
+  }/*
+  async loadPostbyID( post.id) {
+    var test = this.router.get('http://localhost:5000/api/post/'{post.id})
+    return test
+  }*/
+  savePost(formData: FormData) {
+    var post = this.http.post("http://localhost:5000/api/post", formData).subscribe();
+    return post
   }
 }
